84046da94c20404a9d466130ddb099ce40f937cc
nickgs <nickgs1@gmail.com>
2013-10-29 18:04:57 +0000

    Add small scripts to start hipache and drupal 8 images

---
+++ hipache_start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#dockerd is listening on localhost / docker0
+
+# Reverse Proxy Routing / Main Server
+
+#run the main hipache container
+HIPACHE_CONTAINER=$(docker run -d -p 80:80 -p 6379 samalba/hipache /usr/bin/supervisord)
+
+#what port is hipache running on? in case we ever decide to change from -p 80:80
+HIPACHE_PORT=$(docker port $HIPACHE_CONTAINER 80)
+HIPACHE_BRIDGE=$(docker inspect $HIPACHE_CONTAINER | grep Bridge | cut -d":" -f2 | cut -d'"' -f2)
+
+#this is temporary and fails if you're running containers are on disparate docker hosts
+BRIDGE_IP=$(/sbin/ifconfig $HIPACHE_BRIDGE | sed -n '2 p' | awk '{print $2}' | cut -d":" -f2)
+ 
+#get the port that REDIS is running on
+REDIS_PORT=$(docker port $HIPACHE_CONTAINER 6379)
+REDIS_HOST=$BRIDGE_IP
+
+#export the variables to our envrionment
+export HIPACHE_CONTAINER
+export HIPACHE_PORT 
+export HIPACHE_BRIDGE
+export BRIDGE_IP 
+export REDIS_PORT
+export REDIS_HOST 
---
+++ spin_drupal.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#tryrethink.info domain services
+
+#HIPACHE location gets passed in
+REDIS_HOST=$1
+REDIS_PORT=$2
+HIPACHE_PORT=$3
+BRIDGE_IP=$4
+
+#start the container and pass in the necessary environment variables for the app to run
+DRUPAL8_CONTAINER=$(docker run -d -p 80 -e HIPACHE_PORT=$HIPACHE_PORT -e REDIS_HOST=$REDIS_HOST -e REDIS_PORT=$REDIS_PORT -e DOCKER_HOST=$BRIDGE_IP nickgs/drupal8 /usr/bin/supervisord)
+DRUPAL8_HOST=$BRIDGE_IP
+#get the port that the flask app is running on so as to route effectively.
+DRUPAL8_PORT=$(docker port $TRYRETHINK_CONTAINER 80)
+
+# redis takes a bit of time to load up...
+sleep 5
+
+echo $DRUPAL8_CONTAINER
+
+#make sure the main hipache knows about our new http://tryrethink.info domain.`
+redis-cli -h $REDIS_HOST -p $REDIS_PORT rpush frontend:$DRUPAL8_CONTAINER.trydrupal8.dev $DRUPAL8_CONTAINER.trydrupal8
+redis-cli -h $REDIS_HOST -p $REDIS_PORT rpush frontend:$DRUPAL8_CONTAINER.trydrupal8.dev http://$DRUPAL8_HOST:$DRUPAL8_PORT
