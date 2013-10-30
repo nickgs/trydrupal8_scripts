7f081c73f2d66570067cd2299329f02f69d07114
nickgs <nickgs1@gmail.com>
2013-10-30 21:01:53 +0000

    Fix variable name in docker port command

--- spin_drupal.sh
+++ spin_drupal.sh
@@ -10,7 +10,7 @@ BRIDGE_IP=$4
 DRUPAL8_CONTAINER=$(docker run -d -p 80 -e HIPACHE_PORT=$HIPACHE_PORT -e REDIS_HOST=$REDIS_HOST -e REDIS_PORT=$REDIS_PORT -e DOCKER_HOST=$BRIDGE_IP nickgs/drupal8 /usr/bin/supervisord)
 DRUPAL8_HOST=$BRIDGE_IP
 #get the port that the flask app is running on so as to route effectively.
-DRUPAL8_PORT=$(docker port $TRYRETHINK_CONTAINER 80)
+DRUPAL8_PORT=$(docker port $DRUPAL8_CONTAINER 80)
 
 # redis takes a bit of time to load up...
 sleep 5
@@ -19,5 +19,5 @@ sleep 5
 echo $DRUPAL8_CONTAINER
 
 #make sure the main hipache knows about our new http://tryrethink.info domain.`
-redis-cli -h $REDIS_HOST -p $REDIS_PORT rpush frontend:$DRUPAL8_CONTAINER.trydrupal8.dev $DRUPAL8_CONTAINER.trydrupal8
-redis-cli -h $REDIS_HOST -p $REDIS_PORT rpush frontend:$DRUPAL8_CONTAINER.trydrupal8.dev http://$DRUPAL8_HOST:$DRUPAL8_PORT
+redis-cli -h $REDIS_HOST -p $REDIS_PORT rpush frontend:$DRUPAL8_CONTAINER.trydrupal8.com $DRUPAL8_CONTAINER.trydrupal8
+redis-cli -h $REDIS_HOST -p $REDIS_PORT rpush frontend:$DRUPAL8_CONTAINER.trydrupal8.com http://$DRUPAL8_HOST:$DRUPAL8_PORT
