a2ef26b7b22e9a11634dee1265213e73d1b39686
nickgs <nickgs1@gmail.com>
2014-03-04 16:56:54 +0000

    Remove volumes of removed images

--- scripts/rm_instances.sh
+++ scripts/rm_instances.sh
@@ -5,5 +5,5 @@
 docker ps -a | grep testd8 | awk {'if( $7 == "Exit" ) print $1'} | while read line
 do
   logger "[td8] Removing $line.trydrupal8.com"
-  docker rm $line
+  docker rm -v=true $line
 done
