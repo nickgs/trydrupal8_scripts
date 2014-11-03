332232522888209ce57bb9b274353ffe636c29e3
nickgs <nickgs1@gmail.com>
2014-11-03 02:49:31 +0000

    Link hipache container to D8 instances

--- scripts/spin_drupal.sh
+++ scripts/spin_drupal.sh
@@ -7,7 +7,7 @@ HIPACHE_PORT=$3
 BRIDGE_IP=$4
 
 #start the container and pass in the necessary environment variables for the app to run
-DRUPAL8_CONTAINER=$(docker run -d -p 80 -e HIPACHE_PORT=$HIPACHE_PORT -e REDIS_HOST=$REDIS_HOST -e REDIS_PORT=$REDIS_PORT -e DOCKER_HOST=$BRIDGE_IP nickgs/testd8 ./start.sh) 
+DRUPAL8_CONTAINER=$(docker run -d -p 80 --link hip:hip nickgs/testd8 /start.sh) 
 DRUPAL8_HOST=$BRIDGE_IP
 #get the port that the flask app is running on so as to route effectively.
 DRUPAL8_PORT=$(docker port $DRUPAL8_CONTAINER 80)
